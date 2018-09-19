27784af1dfd844bb0df36e972481e3a8abbf6234
Daniel Li <dan@danyll.com>
2018-09-19 12:34:25 +0100

    Add JSDoc

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Returns a promise that resolves once condition is met
+ *
+ * @param {function} condition - Return `true` to cause resolveWhen to resolve
+ * @param {Object} options
+ * @param {Number} options.max - The maximum number of times that `condition` should run
+ * @param {Number} options.interval - Time, in milliseconds, to wait between running `condition`
+ */
 function resolveWhen(condition = () => true, { max, interval = 50 } = {}) {
   return new Promise((resolve, reject) => {
     (function poll(iteration = 1) {
